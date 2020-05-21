771ae41135615e2d6dca12a4a903aefe5622732f
alex-wzm <60631159+alex-wzm@users.noreply.github.com>
2020-05-21 17:36:26 -0400

    formatting and credit

--- typescript-snippets.ts
+++ typescript-snippets.ts
@@ -1,8 +1,11 @@
-/* enum values as list */
+/* ENUM VALUES AS LIST */
 enum E { A, B };
 const keys = Object.keys(E);
 const values = keys.map(k => E[k as any]);
+// credit: https://github.com/microsoft/TypeScript/issues/17198#issuecomment-315400819
 
+// condensed
 const enumAsList = Object.keys(E).map(k => {
     E[k as any];
-}); // condensed
+}); 
+
